894c7e7f29315ae3d12ce30cd248fd02e2175524
Matt <mattfryed@gmail.com>
2019-09-11 14:54:56 -0400

    Scoring
    added scoring script start

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Goal;
+
+
+    void Update()
+    {
+        transform.position = transform.parent.Find("Body").gameObject.transform.position;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.BroadcastMessage("Shrink");
+        }
+    }
+
+
+}
