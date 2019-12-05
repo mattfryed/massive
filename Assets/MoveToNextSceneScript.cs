d30a8ff76460a2c26157b27e5692697aeb3001cc
Nick Santaniello <nick@robysoft.net>
2019-12-04 22:25:13 -0500

    Added basic move to next scene script

---
+++ Assets/MoveToNextSceneScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToNextSceneScript : MonoBehaviour
+{
+
+    public float delayToNextScene = 5f;
+    public int nextSceneNum;
+    private bool movingToNextScene = false;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump_P1_Xbox") || Input.GetButtonDown("Jump_P2_Xbox") || Input.GetButtonDown("Jump_P3_Xbox") || Input.GetButtonDown("Jump_P4_Xbox") || Input.GetButtonDown("Jump_P1") || Input.GetButtonDown("Jump_P2") || Input.GetButtonDown("Jump_P3") || Input.GetButtonDown("Jump_P4"))
+        {
+            Debug.Log("A button was pressed");
+            if (!movingToNextScene)
+            {
+                movingToNextScene = true;
+                Invoke("MoveToNextScene", delayToNextScene);
+            }
+        }
+    }
+
+    void MoveToNextScene()
+    {
+        Application.LoadLevel(nextSceneNum);
+    }
+}
