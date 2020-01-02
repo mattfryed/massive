098db444f18fab1d435b9e742e9cb3b27607ad85
Nick Santaniello <nick@robysoft.net>
2020-01-02 13:13:51 -0500

    Added matter nuggets to additional levels

--- Assets/Scripts/MatterNuggerSpawnScript.cs
+++ Assets/Scripts/MatterNuggerSpawnScript.cs
@@ -7,6 +7,7 @@ public class MatterNuggerSpawnScript : MonoBehaviour
     public GameObject matterNuggetPrefab;
     public float respawnBaseTime = 15f;
     public float variance = 5f;
+    public bool spawnFromParentPosition = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +24,14 @@ public class MatterNuggerSpawnScript : MonoBehaviour
     void SpawnSoon()
     {
         GameObject mn = Instantiate(matterNuggetPrefab);
-        mn.transform.position = new Vector3(0f, 0f, 0f);
+        if (!spawnFromParentPosition)
+        {
+            mn.transform.position = new Vector3(0f, 0f, 0f);
+        }
+        else
+        {
+            mn.transform.position = transform.parent.position;
+        }
 
         mn.GetComponent<Rigidbody>().AddForce(Random.Range(-100.0f, 100.0f), 0f, Random.Range(-100f,100f));
         Invoke("SpawnSoon", Random.Range(respawnBaseTime, respawnBaseTime + variance));
