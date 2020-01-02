8dbc6b8e1d5f46ce08a7fb1aec4a5e8eb8bb4ca2
Nick Santaniello <nick@robysoft.net>
2020-01-02 01:49:21 -0500

    Added mode selector toggle on title screen

---
+++ Assets/ChooseModeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseModeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToNextScene(bool is2v2)
+    {
+        // set a flag here
+        GameObject dm = GameObject.FindWithTag("DataManager");
+        dm.GetComponent<DataManagerScript>().UpdateMode(is2v2);
+        Application.LoadLevel(6);
+    }
+}
