c798e62049a6507f69b7248c082336caa4eda1de
Nick Santaniello <nick@robysoft.net>
2020-01-09 18:22:17 -0500

    Added SFXModule prefab

---
+++ Assets/SfxPlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxPlayerScript : MonoBehaviour
+{
+    // List SFX a player is likely to use here
+    public AudioClip testSFX;
+    private AudioSource AudioS;
+
+    void Start()
+    {
+        AudioS = GetComponent<AudioSource>();
+    }
+
+    void SafePlay(string sfxName)
+    {
+        switch (sfxName)
+        {
+            case "testSFX":
+                if (testSFX)
+                {
+                    AudioS.PlayOneShot(testSFX);
+                }
+                break;
+        }
+        }
+
+    }
