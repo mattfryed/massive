4b8bb0e65794a57ad5feae9a239ea1711009fe7c
Nick Santaniello <nick@robysoft.net>
2020-01-10 02:05:01 -0500

    feature/lil-mass-blobs

---
+++ Assets/MiniMassBlobScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMassBlobScript : MonoBehaviour
+{
+    public GameObject target;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        // Launch in a random direction
+        rb.AddForce(new Vector3(10f, 10f, 10f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // after a delay, move toward target
+
+    }
+}
