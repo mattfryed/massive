46d49341b4b1d43b52800d910a23f4551ad97704
Nick Santaniello <nick@robysoft.net>
2020-01-15 00:59:27 -0500

    Added delay to instructions scene

---
+++ Assets/GoToNextSceneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToNextSceneScript : MonoBehaviour
+{
+    private GameObject dm;
+    public float delayTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dm = GameObject.FindWithTag("DataManager");
+        Invoke("GoToLevel", delayTime);
+    }
+
+    void GoToLevel()
+    {
+        Application.LoadLevel(dm.GetComponent<DataManagerScript>().levelToLoad);
+    }
+
+}
