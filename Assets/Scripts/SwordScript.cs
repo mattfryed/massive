5e8a3fe2545ba2ba97c8d4ac21c525eb0069941a
Nick Santaniello <nick@robysoft.net>
2020-01-16 20:21:10 -0500

    Removed duplicate collider on sword subparticle

--- Assets/Scripts/SwordScript.cs
+++ Assets/Scripts/SwordScript.cs
@@ -9,14 +9,16 @@ public class SwordScript : MonoBehaviour
         if (other.gameObject.tag == "Sword")
         {
             Debug.Log("two swords collided");
-            transform.parent.parent.BroadcastMessage("SwordClash");
+            Debug.Log("as reported by ");
+            Debug.Log(transform.parent.parent.parent.GetComponent<PlayerControllerScript>().playerID);
+            transform.parent.parent.parent.BroadcastMessage("SwordClash");
 
 
         }
         else if (other.gameObject.tag == "Player")
         {   
       //     Debug.Log("Sword colliding with player");
-            transform.parent.parent.BroadcastMessage("Grow");
+            transform.parent.parent.parent.BroadcastMessage("Grow");
             other.gameObject.BroadcastMessage("Shrink", transform.parent.parent.gameObject);
             other.gameObject.BroadcastMessage("playSFX", "struckSFX");
         }
