e660ee4643a91cc27ffcdb9b100b35cc14f1e8e2
Nick Santaniello <nick@robysoft.net>
2020-02-15 16:12:50 -0500

    Added more subtle pootin effect

--- Assets/Scripts/SmallMassBlobScript.cs
+++ Assets/Scripts/SmallMassBlobScript.cs
@@ -8,31 +8,46 @@ public class SmallMassBlobScript : MonoBehaviour
     private bool targetOn = false;
 
     private GameObject child;
-
+    private float timeToAutoKill = 30f;
+    private float bornTime;
     // Start is called before the first frame update
+
+
+    private void Awake()
+    {
+        bornTime = Time.time;
+    }
     void Start()
     {
         child = transform.Find("Particle System").gameObject;
         // Launch in a random direction
-        float ejectPowerX = Random.Range(-250f, 250f);
-        float ejectPowerY = Random.Range(-250f, 250f);
-        if (ejectPowerX >= 0f)
+        if (target != null)
         {
-            ejectPowerX += 20f;
+            float ejectPowerX = Random.Range(-250f, 250f);
+            float ejectPowerY = Random.Range(-250f, 250f);
+            if (ejectPowerX >= 0f)
+            {
+                ejectPowerX += 20f;
+            }
+            else
+            {
+                ejectPowerX -= 20f;
+            }
+            if (ejectPowerY >= 0f)
+            {
+                ejectPowerY += 20f;
+            }
+            else
+            {
+                ejectPowerY -= 20f;
+            }
+            gameObject.GetComponent<Rigidbody>().AddForce(ejectPowerX, 0f, ejectPowerY);
+        } else {
+            float ejectPowerX = Random.Range(-50f, 50f);
+            float ejectPowerY = Random.Range(-50f, 50f);
+           
+            gameObject.GetComponent<Rigidbody>().AddForce(ejectPowerX, 0f, ejectPowerY);
         }
-        else
-        {
-            ejectPowerX -= 20f;
-        }
-        if (ejectPowerY >= 0f)
-        {
-            ejectPowerY += 20f;
-        }
-        else
-        {
-            ejectPowerY -= 20f;
-        }
-        gameObject.GetComponent<Rigidbody>().AddForce(ejectPowerX, 0f, ejectPowerY);
         if (target != null)
         {
             float timeToTarget = Random.Range(.25f, .75f);
@@ -46,7 +61,11 @@ public class SmallMassBlobScript : MonoBehaviour
     }
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if (Time.time >= bornTime + timeToAutoKill)
+        {
+            Destroy(gameObject);
+        }
         //  child.GetComponent<ParticleSystem>().GetComponent<Renderer>().material.SetColor("_Color", new Color(0f, 1f, 0f, .1f));
         if (target != null && targetOn)
         {
